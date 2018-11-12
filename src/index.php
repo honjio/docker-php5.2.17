49169bf96eeb1480b32eb6a935435d9fcaf10406
fujino-yuki <fujino-yuki@dmm.com>
2018-11-12 19:40:34 +0900

    modify : メモ（コメントアウト）の追加

--- src/index.php
+++ src/index.php
@@ -70,6 +70,41 @@
         )
     );
 
+/*
+Memo
+  javaScript
+    文字列の結合
+      +=
+    オブジェクト
+      { 'key': 'string' }
+    変数（example）展開
+      "こんにちは${example}"
+    html文字列内での変数展開
+      '<li class="item">${example}</li>';
+    長い文字列の折り返し
+      `ありがとう
+       こんにちは`;
+    new
+     const func = new Func();
+
+  php
+    文字列の結合
+      .=
+    key 付きの配列
+      array('key' => 'string')
+    通常の変数（$example）展開
+      "こんにちは${example}"
+    html文字列内での変数展開（シングルクオート内で変数は展開されない）
+      "<li class=\"item\">${example}</li>";
+    classで定義された変数（privete $example）展開
+      "こんにちは{$this->example}"
+    長い文字列の折り返し
+      "ありがとう".
+      "こんにちは";
+    new（引数がない場合はなくても良い）
+      $func = new Func;
+*/
+
     class SearchArea {
         private $modelsHtml = '';
         private $html = '';
